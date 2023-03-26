b2ca579e14a5f5e410c4668f46c8f0821b66f456
rjcc <rjcabarong@gmail.com>
2023-03-26 11:23:02 +0800

    first commit

---
+++ models/git-command.js
@@ -0,0 +1,58 @@
+const WorkingDirectory = require("./working-directory");
+const working_directory = new WorkingDirectory();
+
+class GitCommand {
+    constructor(working_directory){
+        this.working_directory = working_directory;
+    }
+    //Command: git init 
+    init(){
+        this.staging = [];
+        this.local_repository = [];
+        return "Initialized as empty Git repository.";
+    }
+
+    //Command: git status
+    status(){      
+        let num_of_changes = Object.keys(this.working_directory.new_changes).length;
+        if(num_of_changes > 0) {
+            return `You have 2 change/s.\n`;
+        }  
+        else {
+            return 'You have 0 change/s.\n';
+        }
+    }
+
+    //Command: git add <filename/file directory/wildcard> 
+    add(path_file){
+        let modified_files = this.working_directory.new_changes;
+        
+        if(modified_files[path_file]){
+            this.staging.push(modified_files[path_file]);
+            delete modified_files[path_file];
+        }
+    }
+
+    //Command: git commit -m "<message>"
+    commit(message){
+        if(this.staging.length > 0){
+            this.local_repository.push({ "message": message, "files": this.staging });
+            this.staging = [];
+            return "Done committing to local repository.";
+        }
+        return "Nothing to commit.";
+    }
+
+    //Command: git push
+    push(){   
+        if(this.local_repository.length > 0){
+            return "Done pushing to remote repository.";
+        } 
+        else {
+            return "Nothing to push. No committed file found.";
+        }     
+    }
+}
+
+
+module.exports = GitCommand;
